af79621441418ae7dd7581d4f8c967309a21717c
Cheng Shi <sc971008@gmail.com>
2023-03-25 14:20:21 -0400

    An updated ImageController And two command class

--- Assignment5/core/src/ime/control/ImageController.java
+++ Assignment5/core/src/ime/control/ImageController.java
@@ -1,5 +1,17 @@
 package ime.control;
 
+import ime.control.command.Brighten;
+import ime.control.command.Dither;
+import ime.control.command.Filter;
+import ime.control.command.Greyscale;
+import ime.control.command.Hflip;
+import ime.control.command.Load;
+import ime.control.command.RgbCombine;
+import ime.control.command.RgbSplit;
+import ime.control.command.Save;
+import ime.control.command.Vflip;
+import ime.model.ImageProcessor;
+import ime.model.PpmProcessor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,17 +24,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.Set;
-
-import ime.control.command.Brighten;
-import ime.control.command.Greyscale;
-import ime.control.command.Hflip;
-import ime.control.command.Load;
-import ime.control.command.RgbCombine;
-import ime.control.command.RgbSplit;
-import ime.control.command.Save;
-import ime.control.command.Vflip;
-import ime.model.ImageProcessor;
-import ime.model.PpmProcessor;
 
 
 /**
@@ -115,12 +116,6 @@ public class ImageController implements IController {
           }
           cmd = new Brighten(Integer.parseInt(args.get(0)), args.get(1), args.get(2));
           break;
-        case "greyscale":
-          if (args.size() != 3) {
-            throw wnaE;
-          }
-          cmd = new Greyscale(args.get(0), args.get(1), args.get(2));
-          break;
         case "vertical-flip":
           if (args.size() != 2) {
             throw wnaE;
@@ -133,6 +128,29 @@ public class ImageController implements IController {
           }
           cmd = new Hflip(args.get(0), args.get(1));
           break;
+        // color Trans
+        case "greyscale":
+        case "sepia":
+          if (args.size() != 2 && args.size() != 3) {
+            throw wnaE;
+          }
+          cmd = args.size() == 2 ?
+              new Greyscale(in, args.get(0), args.get(1))
+              : new Greyscale(args.get(0), args.get(1), args.get(2));
+          break;
+        // filter
+        case "blur":
+        case "sharpen":
+          if (args.size() != 2) {
+            throw wnaE;
+          }
+          cmd = new Filter(in, args.get(0), args.get(1));
+          break;
+        case "dither":
+          if (args.size() != 2) {
+            throw wnaE;
+          }
+          cmd = new Dither(args.get(0),args.get(1));
         default:
           output.append("!<Error>!: \t" + String.format("Unknown command [%s]", in) + "\n");
           cmd = null;
@@ -147,9 +165,11 @@ public class ImageController implements IController {
         cmd.execute(model);
         output.append("Executed: \t").append(command).append("\n");
       } catch (IllegalArgumentException | IllegalStateException iae) {
-        output.append("!<Error>!: \t" + iae.toString() + "\n");
-      } catch (FileNotFoundException fne){
-        output.append("!<Error>!: \t" + fne.toString().replaceAll("The system cannot find the path specified","No such file or directory") + "\n");
+        output.append("!<Error>!: \t" + iae + "\n");
+      } catch (FileNotFoundException fne) {
+        output.append("!<Error>!: \t" + fne.toString()
+            .replaceAll("The system cannot find the path specified", "No such file or directory")
+            + "\n");
       }
     }
 
@@ -212,11 +232,11 @@ public class ImageController implements IController {
     int fileOption = Arrays.asList(args).indexOf("-file");
 
     //cmd line option
-    if(args.length>0 && fileOption!=-1){
-      IController ctrl = new ImageController(new StringReader("run "+args[fileOption+1]), System.out);
+    if (args.length > 0 && fileOption != -1) {
+      IController ctrl = new ImageController(new StringReader("run " + args[fileOption + 1]),
+          System.out);
       ctrl.run(new PpmProcessor());
-    }
-    else {
+    } else {
       //Example main program
       IController ctrl = new ImageController(new InputStreamReader(System.in), System.out);
       ctrl.run(new PpmProcessor());
